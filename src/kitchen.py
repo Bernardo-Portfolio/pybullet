9ce7d1dd022d5ac552a1922ef6dc706b1e9c59d4
Bernardo <bernardo.dias@mov.ai>
2024-03-26 12:09:27 +0000

    Update script to show camera and joint_vel

--- src/kitchen.py
+++ src/kitchen.py
@@ -4,6 +4,8 @@ import pybullet as p
 import pybullet_data  
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
 
 
 
@@ -21,7 +23,7 @@ class Kitchen:
         self.plate = p.loadURDF(model_path +'/models/kitchenware/plate/plate.urdf',[1.0,0.3,1.0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=False)
         startPos = [0,0,1]
         startOrientation = p.getQuaternionFromEuler([0,0,0])
-        boxId = p.loadURDF(model_path +'/models/r2d2/r2d2.urdf',startPos, startOrientation) 
+        self.boxId = p.loadURDF(model_path +'/models/r2d2/r2d2.urdf',startPos, startOrientation) 
         self.drawer_to_joint_id = {1: 18, 
                                    2: 22, 
                                    3: 27, 
@@ -45,16 +47,11 @@ class Kitchen:
                                        10: (0.0, 0.4), 
                                        11: (0, 1.57)}
         
-        robot_nr_joints = p.getNumJoints(boxId)
-        for i in range(0, robot_nr_joints):
-            info = p.getJointInfo(boxId,i)
-        camera = p.getJointInfo(boxId, 15) 
-        head = p.getJointInfo(boxId, 13)
-         
-        
-        _, joint_name, _, _, _, _, _, _, _, _, _, _, _, _, pos_camera, ori_camera, _ = p.getJointInfo(boxId, 15)
-        _, joint_name, _, _, _, _, _, _, _, _, _, _, _, _, pos_head, ori_head, _ = p.getJointInfo(boxId, 13)
-        self.camera_pos = [0,0.1,1.3]
+        # robot_nr_joints = p.getNumJoints(boxId)
+        # for i in range(0, robot_nr_joints):
+        #     info = p.getJointInfo(boxId,i)
+        #     print(info)
+
         # joint_name = joint_name.decode("utf8")
         # part_name = part_name.decode("utf8")
     def open_drawer(self, drawer_id):
@@ -79,57 +76,87 @@ if __name__ == '__main__':
     p.setRealTimeSimulation(1)
     p.setGravity(0, 0, -9.81)
     p.setPhysicsEngineParameter(enableConeFriction=0)
+    distance = 10000
+    img_w, img_h = 120, 80
 
+    
+    wheels = [2,3]
+    steering = [4, 6]
 
-   
-
-    # width = 128
-    # height = 128
-    # fov = 60
-    # aspect = width / height
-    # near = 0.02
-    # far = 1
-
-    # view_matrix = p.computeViewMatrix([0, 0, 0.5], [0, 0, 0], [1, 0, 0])
-    # projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, near, far)
-
-    # images = p.getCameraImage(width,
-    #                         height,
-    #                         view_matrix,
-    #                         projection_matrix,
-    #                         shadow=True,
-    #                         renderer=p.ER_TINY_RENDERER)
-    # depth_buffer_tiny = np.reshape(images[3], [width, height])
-    # depth_tiny = far * near / (far - (far - near) * depth_buffer_tiny)
-    # rgb_tiny = np.reshape(images[2], (height, width, 4)) * 1. / 255.
-    # seg_tiny = np.reshape(images[4], [width, height]) * 1. / 255.
-    # time.sleep(1)
-    # plt.subplot()
-    # plt.imshow(seg_tiny)
-    # plt.title('Seg Tiny')
-    # plt.show()
-
+    targetVelocitySlider = p.addUserDebugParameter("wheelVelocity", -10, 10, 0)
+    maxForceSlider = p.addUserDebugParameter("maxForce", 0, 10, 10)
+    steeringSlider = p.addUserDebugParameter("steering", -0.5, 0.5, 0)
+    
+ 
 
     #initialize kitchen object
     kitchen = Kitchen()
-    p.resetDebugVisualizerCamera(cameraDistance=0.0, cameraYaw=0, cameraPitch=0, cameraTargetPosition=kitchen.camera_pos)
-    img = p.getCameraImage(224, 224, renderer=p.ER_BULLET_HARDWARE_OPENGL)
-    time.sleep(30)
-    #open all drawers
-    for i in range(10):
-        drawer_id = i+1
-        kitchen.open_drawer(drawer_id)
-        time.sleep(1)
 
-    time.sleep(10)
+   
+    # camera = p.getJointInfo(kitchen.boxId, 15) 
+    # head = p.getJointInfo(kitchen.boxId, 13)
+    # _, joint_name, _, _, _, _, _, _, _, _, _, _, _, _, pos_camera, ori_camera, _ = p.getJointInfo(kitchen.boxId, 15)
+    # _, joint_name, _, _, _, _, _, _, _, _, _, _, _, _, pos_head, ori_head, _ = p.getJointInfo(kitchen.boxId, 13)
 
-    #close all drawers
-    for i in range(10):
-        drawer_id = i+1
-        kitchen.close_drawer(drawer_id)
-        time.sleep(1)
 
-    time.sleep(10)
+
+    
+    p.addUserDebugText()
+    # for wheel in inactive_wheels:
+    #     p.setJointMotorControl2(kitchen.boxId, wheel, p.VELOCITY_CONTROL, targetVelocity=0, force=0)
+    while True:
+        robot_pos, robot_ori = p.getBasePositionAndOrientation(kitchen.boxId)
+        robot_yaw = p.getEulerFromQuaternion(robot_ori)[-1]
+        xA, yA, zA = robot_pos
+        yA += 0.1
+        zA += 0.3
+        
+        xB = xA + math.cos(robot_yaw)*distance
+        yB = yA + math.sin(robot_yaw)*distance
+        zB = zA
+
+        view_matrix = p.computeViewMatrix(cameraEyePosition=[xA,yA,zA],
+                                          cameraTargetPosition=[xB,yB,zB],
+                                          cameraUpVector=[0,0,1.0])
+        
+        projection_matrix = p.computeProjectionMatrixFOV(fov=90, aspect=1.0, nearVal=0.02, farVal=3.5)
+
+        img = p.getCameraImage(img_w, img_h, view_matrix, projection_matrix, shadow=True, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+
+        maxForce = p.readUserDebugParameter(maxForceSlider)
+        targetVelocity = p.readUserDebugParameter(targetVelocitySlider)
+        steeringAngle = p.readUserDebugParameter(steeringSlider)
+        #print(targetVelocity)
+
+        for wheel in wheels:
+            _,joint_vel,_,_ = p.getJointState(kitchen.boxId, wheel)
+            s = ''.join(str(x) for x in joint_vel)
+            p.addUserDebugText(s, [1,1,1])
+            
+            p.setJointMotorControl2(kitchen.boxId,
+                                    wheel,
+                                    p.VELOCITY_CONTROL,
+                                    targetVelocity=targetVelocity,
+                                    force=maxForce)
+
+        p.resetDebugVisualizerCamera(cameraDistance=0.0, cameraYaw=0, cameraPitch=0, cameraTargetPosition=kitchen.camera_pos)
+        #img = p.getCameraImage(224, 224, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+        time.sleep(30)
+        #open all drawers
+        for i in range(10):
+            drawer_id = i+1
+            kitchen.open_drawer(drawer_id)
+            time.sleep(1)
+
+        time.sleep(10)
+
+        #close all drawers
+        for i in range(10):
+            drawer_id = i+1
+            kitchen.close_drawer(drawer_id)
+            time.sleep(1)
+
+        time.sleep(10)
 
 
 
